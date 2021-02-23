b40cd34c161770cc29ef7f79d9d2aded4c8950fd
Davis Catherman <daviscatherman@gmail.com>
2021-02-23 13:10:01 -0500

    Changed state space - added angle history queue - returns x,y,theta,dx,dy,dtheta

--- simulator/Agent.py
+++ simulator/Agent.py
@@ -17,9 +17,7 @@ class Agent:
         self.start_pos = start_pos
         self.pos_hist_len = pos_hist_len
         self.pos_history = Queue(maxsize=pos_hist_len)
-
-        for i in range(pos_hist_len):
-            self.pos_history.put(self.start_pos, block=False)
+        self.ang_history = Queue(maxsize=pos_hist_len)
 
         self.mass = mass
         self.radius = radius
@@ -32,6 +30,12 @@ class Agent:
         self.shape.elasticity = .5
         self.shape.friction = 0.99
 
+        for i in range(pos_hist_len):
+            self.pos_history.put(self.start_pos, block=False)
+
+        for i in range(pos_hist_len):
+            self.ang_history.put(self.get_angle(), block=False)
+
     def get_body(self) -> object:
         return self.body
 
@@ -41,11 +45,21 @@ class Agent:
     def get_state(self) -> np.ndarray:
         self.pos_history.get(block=False)
         self.pos_history.put(self.get_pos(), block=False)
+        self.ang_history.get(block=False)
+        self.ang_history.put(self.get_angle(), block=False)
+
+        pos_hist = list(self.pos_history.queue)
+        delta1 = pos_hist[-1] - pos_hist[0]
+        ang_hist = list(self.ang_history.queue)
+        delta2 = ang_hist[-1] - ang_hist[0]
 
         observation = self._sense()
         # state = np.append(observation, list(self.pos_history.queue))
 
-        return observation
+        state = np.append(observation, delta1)
+        state = np.append(state, delta2)
+
+        return state
 
     def set_motors(self, velocities: tuple) -> None:
         self.body.apply_force_at_local_point((20000 * velocities[0], 0), (0, -20))
