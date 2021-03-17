c5ad89628f02a1e1f5e81923052b52475c5325b9
Davis Catherman <dscatherman@wpi.edu>
2021-03-17 11:31:38 -0400

    Configurable folder as argument - calculates avg reward over a sliding window

--- hpc/collect_results.py
+++ hpc/collect_results.py
@@ -5,24 +5,46 @@ import os
 import time
 import itertools
 
+import argparse
+
 import numpy as np
 
 np.set_printoptions(precision=1, suppress=True)
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="Result collector")
+    # parser.add_argument('-T', '--train_dqn',    action='store_true',    help='Whether train mode')
+    parser.add_argument('-f',   '--folder',   dest="FOLDER",  type=str,   help='Folder to process')
+
+    args = parser.parse_args()
+    return args
 
 def f(d):
     # with open('{}/data_values.csv') as csvFile:
     #     reader = csv.reader(csvFile, delimiter=',')
     arr = np.genfromtxt('{}/data_values.csv'.format(d),delimiter=',')
-    arr = arr.tolist()
-    arr = sorted(arr, reverse=True, key=lambda e: e[1])
 
-    return d, arr
+    n = 20
+
+    max_avg = -9E9
+    for i in range(n, arr.shape[0]):
+        a = np.sum(arr[i-n:i,1])/n
+        max_avg = max(max_avg, a)
+
+    # arr = arr.tolist()
+    
+    # arr = sorted(arr, reverse=True, key=lambda e: e[1])
+
+    return d, max_avg
 
 
 
 
 def main():
-    base_dir = 'runs/2021.03.11__18.07.45'
+    args = parse()
+
+    base_dir = args.FOLDER
 
     # dirs = os.listdir(base_dir)
     # print(dirs)
@@ -42,18 +64,35 @@ def main():
     # best = itertools.accumulate(res, fx)
     # print(list(best))
 
-    print(res)
+    # print(res)
 
-    best = ("", np.array([[9E9, -9E9]]))
+    # best = ("", np.array([[9E9, -9E9]]))
+    # for e in res:
+    #     if e[1][0][1] > best[1][0][1]:
+    #         best = e
+
+    res.sort(key=lambda e: e[1], reverse=True)
+
+    print("---------")
+    print("---------")
+
+    for e in range(0,5):
+        print(res[e])
+
+    print("---------")
+    print("---------")
+
+    best = ("", -9E9)
     for e in res:
-        if e[1][0][1] > best[1][0][1]:
+        if e[1] > best[1]:
             best = e
 
     print("---------")
     print("---------")
 
+    print(best)
     print(best[0])
-    print(best[1][0])
+    print(best[1])
 
 
 
