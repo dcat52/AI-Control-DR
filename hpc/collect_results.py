b5c1e1ca31aa87760d4e70a492f255ee06cca3db
Davis Catherman <dscatherman@wpi.edu>
2021-03-19 16:54:45 -0400

    Added collect results script -needs formalizing. - Updates to task script - updates to job sbatch

--- hpc/collect_results.py
+++ hpc/collect_results.py
@@ -8,21 +8,23 @@ import itertools
 import argparse
 
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d
+from matplotlib import cm
 
-np.set_printoptions(precision=1, suppress=True)
+matplotlib.use("Agg")
+np.set_printoptions(precision=2, linewidth=180, suppress=True)
 
 
 def parse():
     parser = argparse.ArgumentParser(description="Result collector")
-    # parser.add_argument('-T', '--train_dqn',    action='store_true',    help='Whether train mode')
     parser.add_argument('-f',   '--folder',   dest="FOLDER",  type=str,   help='Folder to process')
 
     args = parser.parse_args()
     return args
 
 def f(d):
-    # with open('{}/data_values.csv') as csvFile:
-    #     reader = csv.reader(csvFile, delimiter=',')
     arr = np.genfromtxt('{}/data_values.csv'.format(d),delimiter=',')
 
     n = 20
@@ -46,36 +48,15 @@ def main():
 
     base_dir = args.FOLDER
 
-    # dirs = os.listdir(base_dir)
-    # print(dirs)
-
     dirs = glob.glob('{}/*/'.format(base_dir))
-    print(dirs)
     
     res = []
     with multiprocessing.Pool() as p:
         res = p.map(f, dirs)
-    
-    # def fx(out, val):
-    #     print(out)
-    #     if val[1][0] < out[1][0]:
-    #         return (val)
-
-    # best = itertools.accumulate(res, fx)
-    # print(list(best))
-
-    # print(res)
-
-    # best = ("", np.array([[9E9, -9E9]]))
-    # for e in res:
-    #     if e[1][0][1] > best[1][0][1]:
-    #         best = e
 
     res.sort(key=lambda e: e[1], reverse=True)
 
-    print("---------")
-    print("---------")
-
+    print("TOP 5:")
     for e in range(0,5):
         print(res[e])
 
@@ -87,13 +68,58 @@ def main():
         if e[1] > best[1]:
             best = e
 
+    # print(best)
+    print("BEST FOLDER: ", best[0])
+    print("BEST SCORE: ", best[1])
+
+    res.sort(key=lambda e: e[0], reverse=False)
+
+    y = range(1,21)
+    x = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00]
+    
+    y = [50000, 40000, 30000, 20000, 10000, 5000, 3000, 1000]
+    x = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+    x = np.array(range(1,101))/100
+    y = range(1,21)
+    
+    z = []
+    z2 = []
+    for e in res:
+        z.append(e[1])
+        z2.append(e[0])
+
+    z = np.reshape(z, (len(y), len(x)))
+    z2 = np.reshape(z2, (len(y), len(x)))
+
+    x = np.array(x)
+    y = np.array(y)
+    x, y = np.meshgrid(x, y)
+
+    np.save('x.npy', x)
+    np.save('y.npy', y)
+    np.save('z.npy', z)
+
+    # z = np.gaussian_filter(z, sigma=1)
+
     print("---------")
     print("---------")
+    print(x[0:3,0:3])
+    print("---------")
+    print(y[0:3,0:3])
+    print("---------")
+    print(z2[0:3,0:3])
 
-    print(best)
-    print(best[0])
-    print(best[1])
+    plt.ioff()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(x, y, z, rcount=100, ccount=100, edgecolor='none', cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
 
+    plt.savefig("out.png", dpi=400)
 
 
 if __name__=='__main__':
--- hpc/task_script.sh
+++ hpc/task_script.sh
@@ -10,7 +10,9 @@ echo store loc: $3
 module load singularity
 
 singularity exec ./hpc/output.sif python main.py \
-    --train --episodes 1000 --save_prefix $3/$1/data \
-    --print_freq 50 --save_freq 200 --write_freq 10 $2
+    --train --episodes 1200 --save_prefix $3/$1/data \
+    --print_freq 50 --save_freq 200 --write_freq 5 \
+    --std 0.10 --theta 0.16 --update_freq 8 --tau 0.40 \
+    --gamma 0.99 $2
 
 echo "Time Running (s): $SECONDS for task $1"
