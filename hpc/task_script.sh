2da2d85eff4d499e3fe467c85257e91e888b5d71
Davis Catherman <dscatherman@wpi.edu>
2021-03-22 11:19:34 -0400

    Add more default params to task_script, spreading them out for readability

--- hpc/task_script.sh
+++ hpc/task_script.sh
@@ -13,8 +13,10 @@ singularity exec ./hpc/output.sif python main.py \
     --train --episodes 1200 --save_prefix $3/$1/data \
     --print_freq 50 --save_freq 200 --write_freq 5 \
     --seed 539 \
-    --std 0.10 --theta 0.16 \
-    --update_freq 8 --tau 0.40 \
+    --std 0.10 --theta 0.10 \
+    --update_freq 2 --tau 0.015 \
+    --batch_size 32 --buffer_capacity 10000 \
+    --actor_lr 0.0001 --critic_lr 0.0002 \
     --gamma 0.99 $2
 
 echo "Time Running (s): $SECONDS for task $1"
