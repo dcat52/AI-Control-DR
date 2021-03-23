b5a8744186e131b97b92e739fe2611cf84c54905
Davis Catherman <dscatherman@wpi.edu>
2021-03-23 02:22:57 -0400

    cleanup graph code

--- hpc/collect_results.py
+++ hpc/collect_results.py
@@ -37,10 +37,6 @@ def f(d):
         a = np.sum(arr[i-n:i,1])/n
         max_avg = max(max_avg, a)
 
-    # arr = arr.tolist()
-    
-    # arr = sorted(arr, reverse=True, key=lambda e: e[1])
-
     return d, max_avg
 
 def main():
@@ -85,12 +81,6 @@ def main():
     print("BEST SCORE: ", best[1])
 
     res.sort(key=lambda e: e[0], reverse=False)
-
-    # y = range(1,21)
-    # x = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00]
-    
-    # y = [50000, 40000, 30000, 20000, 10000, 5000, 3000, 1000]
-    # x = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
     x = np.array(list(range(1, 5)))
     y = np.array(list(range(1, 100, 2))) / 100
@@ -137,58 +127,9 @@ def main():
 
     def plot(x, y, z, filename, angle=None):
 
-        def draw_sphere2(xCenter, yCenter, zCenter, r, ax):
-            #draw sphere
-            u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-            x=np.cos(u)*np.sin(v)
-            y=np.sin(u)*np.sin(v)
-            z=np.cos(v)
-            # shift and scale sphere
-            # xlen = (max(xarr)-min(xarr))/10
-            # ylen = (max(yarr)-min(yarr))/10
-            # zlen = (np.max(np.max(z))-np.min(np.min(z)))/10
-
-            xmin, xmax = ax.get_xlim()
-            xlen = (xmax-xmin)/100
-            ymin, ymax = ax.get_ylim()
-            ylen = (ymax-ymin)/100
-            zmin, zmax = ax.get_zlim()
-            zlen = (zmax-zmin)/100
-
-            print(xlen, ylen, zlen)
-
-            x = r*xlen*x + xCenter
-            y = r*ylen*y + yCenter
-            z = r*zlen*z + zCenter
-            return (x,y,z)
-
-        # def draw_crosshair(xCenter, yCenter, zCenter, r):
-        #     #draw sphere
-        #     xlen = (max(xarr)-min(xarr))/10
-        #     ylen = (max(yarr)-min(yarr))/10
-        #     x, y = np.mgrid[0:xlen, 0:ylen]
-        #     z = np.zeros((xlen,ylen))
-        #     z[:, ylen//2] = 50
-        #     z[xlen//2, :] = 50
-        #     return (x,y,z)
-
-
-        # xpt = 10000
-        # ypt = 32
-        # zpt = z[np.where(yarr == ypt), np.where(xarr == xpt)][0][0]
-
         plt.ioff()
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        # ax.plot([xpt], [ypt], [zpt], 'go', alpha=0.5)
-
-        # xmin, xmax = min(xarr), max(xarr)
-        # ymin, ymax = min(yarr), max(yarr)
-        # zmin, zmax = (-50,80)
-        # c = np.linspace(zmin,zmax,100)
-        # a = np.ones(c.shape)*xpt
-        # b = np.ones(c.shape)*ypt
-        # ax.plot(a,b,c)
 
 
         ax.plot_surface(x, y, z, rstride=1, cstride=1, edgecolor='none', cmap=cm.coolwarm,
@@ -197,16 +138,6 @@ def main():
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
-
-        # print(xpt, ypt, zpt)
-
-        # print("WHERE:", np.where(yarr == 32), np.where(xarr == 10000))
-        # ax.plot(xpt, ypt, zpt, 'ro', alpha=0.5)
-
-        # (xs,ys,zs) = draw_crosshair(10000, 32, z[ypt[0]//400, xpt[0]//4000], 20)
-        # print(xs, ys, zs)
-
-        # ax.plot_wireframe(xs, ys, zs, color="r")
 
         if angle is not None:
             ax.view_init(angle[0], angle[1]) 
